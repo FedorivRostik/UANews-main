248db6201d48c2ea8662e91db25860f65761751d
Rostyslav Fedoriv <froizerner@gmail.com>
2023-05-23 01:55:46 +0300

    added email logic

---
+++ js/email.js
@@ -0,0 +1,8 @@
+const emailRef = document.getElementById('email');
+
+const sendEmail = function () { window.open('mailto:froizerner@gmail.com?subject=I have information for you&body=Write here'); };
+
+emailRef.addEventListener('click', function () {
+    
+    window.open('mailto:froizerner@gmail.com?subject=I have information for you&body=Write here');
+ })
